ee668a3b166627df081eaa15086aa542ee763dfc
hiroto-okumura <hiroto_okumura@yahoo.co.jp>
2024-06-04 21:27:19 +0900

    add module Node-scss, add base-file Sidebar comp:tsx,scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* sidevar */}
+      <Sidebar />
+      {/* home */}
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.scss";
+const Sidebar = () => {
+  return (
+    <div>
+      {/* Sidebar Left */}
+      <div className="sidebar-left"></div>
+      {/* Sidbar Right */}
+      <div className="sidebar-right"></div>
+    </div>
+  );
+};
+
+export default Sidebar;
